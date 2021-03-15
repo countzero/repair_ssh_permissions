d28dfa6359e7d1bd6ed32d99cec40cfb651a3520
Finn Kumkar <finn.kumkar@gmail.com>
2021-03-15 23:00:56 +0100

    Work on setting permissions according to best practices.

--- repair_ssh_permissions.ps1
+++ repair_ssh_permissions.ps1
@@ -14,7 +14,7 @@ Specifies the path to the .ssh directory.
 .\repair_ssh_permissions.ps1
 
 .EXAMPLE
-.\repair_ssh_permissions.ps1 -path "C:\Users\Finn Kumkar\.ssh"
+.\repair_ssh_permissions.ps1 -path "C:\Users\John Doe\.ssh"
 
 #>
 
@@ -27,5 +27,69 @@ Param (
     $path
 )
 
-Write-Host "Not yet implemented..."
-Write-Host "${path}"
+
+# Default the target directory path to '%USERPROFILE%/.ssh'.
+if (!$path) {
+    $path = $(Join-Path -Path "$env:USERPROFILE" -ChildPath ".ssh")
+}
+
+function Disable-Inheritance([String] $item) {
+
+    Write-Host "Disable inheritance on '${item}'..."
+
+    $acl = Get-Acl -Path $item
+    $acl.SetAccessRuleProtection($true, $true)
+    Set-Acl -Path $item -AclObject $acl
+}
+
+function Remove-AllAccessPermissions([String] $item) {
+
+    Write-Host "Removing all access permissions on '${item}'..."
+
+    $acl = Get-Acl -Path "$item"
+
+    ForEach($accessRule in $acl.Access) {
+        $acl.RemoveAccessRule($accessRule) | Out-Null
+    }
+
+    Set-Acl -Path $item -AclObject $acl
+}
+
+function Grant-CurrentUserFullControl([String] $item) {
+
+    $currentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+
+    Write-Host "Granting '$currentUser' full control over '${item}'..."
+
+    $acl = Get-Acl -Path $item
+
+    $accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule(
+        $currentUser,
+        "FullControl",
+        "Allow"
+    )
+
+    $acl.SetAccessRule($accessRule)
+
+    Set-Acl -Path $item -AclObject $acl
+}
+
+
+Write-Host "Fixing directory and file permissions of '${path}'..."
+
+$directories = @(Get-ChildItem -Path $path -Directory -Recurse).FullName
+
+foreach ($directory in $directories) {
+
+    Disable-Inheritance -item $directory
+    Remove-AllAccessPermissions -item $directory
+    Grant-CurrentUserFullControl -item $directory
+}
+
+#
+# TODO:
+#
+#     1. Include the root directory
+#     2. Include all files within
+#     3. Group all items and loop once
+#
